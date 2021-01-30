239a678e64b54a225e3f2053194ed80474d1b71c
a-poor <45295232+a-poor@users.noreply.github.com>
2021-01-29 20:52:31 -0500

    Updating docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,17 +4,21 @@ using Documenter, FeatureEng
 
 makedocs(
     modules = [FeatureEng],
-    sitename="FeatureEng",
+    sitename="FeatureEng.jl Docs",
+    authors="Austin Poor",
+    clean=true,
+    debug=true,
     pages=[
         "index.md"
-    ]
+    ],
+    repo   = "github.com/a-poor/FeatureEng.jl.git",
 )
 
 deploydocs(
     root   = dirname(abspath(PROGRAM_FILE)),
     # target = "build",
     repo   = "github.com/a-poor/FeatureEng.jl.git",
-    branch = "gh-pages",
+    # branch = "gh-pages",
     # deps   = nothing | <Function>,
     # make   = nothing | <Function>,
     devbranch = "main",
