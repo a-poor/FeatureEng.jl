62fb5ede7c98bdfb7892bbfa6d6ea6ff5038cd5a
a-poor <45295232+a-poor@users.noreply.github.com>
2021-01-29 19:10:22 -0500

    Updating doc files

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,23 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, FeatureEng
 
-makedocs(sitename="FeatureEng")
+makedocs(
+    sitename="FeatureEng"
+)
+
+deploydocs(
+    root   = abspath(PROGRAM_FILE),
+    target = "build",
+    repo   = "github.com/a-poor/FeatureEng.jl.git",
+    branch = "gh-pages",
+    # deps   = nothing | <Function>,
+    # make   = nothing | <Function>,
+    devbranch = "main",
+    devurl = "dev",
+    # versions = ["stable" => "v^", "v#.#", devurl => devurl],
+    versions = ["stable" => "v^", "v#.#"],
+    push_preview    = false,
+    # repo_previews   = repo,
+    # branch_previews = branch,
+)
 
