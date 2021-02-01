07f9b08d832df4ebcdc69a7a07f75731455a8cf7
a-poor <45295232+a-poor@users.noreply.github.com>
2021-02-01 17:22:30 -0500

    Updating github actions

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,10 @@ using Documenter, FeatureEng
 
 makedocs(
     modules = [FeatureEng],
-    sitename="FeatureEng.jl Docs",
+    sitename="FeatureEng.jl",
     authors="Austin Poor",
-    clean=true,
-    debug=true,
+    # clean=true,
+    # debug=true,
     pages=[
         "Main" => "index.md",
         # "Getting Started" => "getting-started.md",
@@ -18,17 +18,17 @@ makedocs(
 )
 
 deploydocs(
-    root   = dirname(abspath(PROGRAM_FILE)),
+    # root   = dirname(abspath(PROGRAM_FILE)),
     # target = "build",
     repo   = "github.com/a-poor/FeatureEng.jl.git",
     # branch = "gh-pages",
     # deps   = nothing | <Function>,
     # make   = nothing | <Function>,
-    devbranch = "main",
+    # devbranch = "main",
     # devurl = "",
     # versions = ["stable" => "v^", "v#.#", devurl => devurl],
     # versions = ["stable" => "v^", "v#.#"],
-    push_preview    = true,
+    # push_preview    = true,
     # repo_previews   = repo,
     # branch_previews = branch,
 )
