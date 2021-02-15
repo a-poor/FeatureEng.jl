d7a8b0aa87ae6e74e3e6fa9eeb10a3fe7da5fb82
a-poor <45295232+a-poor@users.noreply.github.com>
2021-02-15 11:06:38 -0500

    Updating docs & types in impute

--- src/impute.jl
+++ src/impute.jl
@@ -6,6 +6,26 @@ abstract type AbstractImpute end
 
 [Impute](https://en.wikipedia.org/wiki/Imputation_(statistics)) missing data 
 using the median value.
+
+# Examples:
+
+```julia-repl
+julia> data = [1,2,3,4,missing,5];
+julia> imp = fit_transform!(ImputeMedian(),data)
+ImputeMedian
+  data_median: Float64 3.0
+  is_fit: Bool true
+julia> apply_transform(imp,data)
+6-element Array{Int64,1}:
+  1
+  2
+  3
+  4
+  3
+  5  
+```
+
+See also: [`ImputeMean`](@ref)
 """
 @with_kw mutable struct ImputeMedian <: AbstractImpute
     data_median::Float64 = 0.0
@@ -16,8 +36,10 @@ end
     fit_transform!(imp::ImputeMedian, data::T) where T <: AbstractArray{<:Number}
 
 Fit an `ImputeMedian` object to `data`.
+
+See also: [`ImputeMedian`](@ref)
 """
-function fit_transform!(imp::ImputeMedian, data::T) where T <: AbstractArray{<:Number}
+function fit_transform!(imp::ImputeMedian, data::T) where T <: AbstractArray{<:Union{Missing,<:Number}}
     imp.data_median = median(skipmissing(data))
     imp.is_fit = true
     imp
@@ -27,8 +49,10 @@ end
     apply_transform(imp::ImputeMedian, data::T) where T <: AbstractArray{<:Number}
 
 Imputes missing values in `data` using a fit `ImputeMedian` object.
+
+See also: [`ImputeMedian`](@ref)
 """
-function apply_transform(imp::ImputeMedian, data::T) where T <: AbstractArray{<:Number}
+function apply_transform(imp::ImputeMedian, data::T) where T <: AbstractArray{<:Union{Missing,<:Number}}
     if !imp.is_fit
         error("`imp` must be fit first.")
     end
@@ -37,10 +61,12 @@ end
 
 
 """
-ImputeMean <: AbstractImpute
+    ImputeMean <: AbstractImpute
 
 [Impute](https://en.wikipedia.org/wiki/Imputation_(statistics)) missing data 
 using the mean value.
+
+See also: [`ImputeMean`](@ref)
 """
 @with_kw mutable struct ImputeMean <: AbstractImpute
     data_mean::Float64 = 0.0
@@ -51,8 +77,10 @@ end
     fit_transform!(imp::ImputeMean, data::T) where T <: AbstractArray{<:Number}
 
 Fit an `ImputeMean` object to `data`.
+
+See also: [`ImputeMean`](@ref)
 """
-function fit_transform!(imp::ImputeMean, data::T) where T <: AbstractArray{<:Number}
+function fit_transform!(imp::ImputeMean, data::T) where T <: AbstractArray{<:Union{Missing,<:Number}}
     imp.data_mean = mean(skipmissing(data))
     imp.is_fit = true
     imp
@@ -62,8 +90,10 @@ end
     apply_transform(imp::ImputeMean, data::T) where T <: AbstractArray{<:Number}
 
 Imputes missing values in `data` using a fit `ImputeMean` object.
+
+See also: [`ImputeMean`](@ref)
 """
-function apply_transform(imp::ImputeMean, data::T) where T <: AbstractArray{<:Number}
+function apply_transform(imp::ImputeMean, data::T) where T <: AbstractArray{<:Union{Missing,<:Number}}
     if !imp.is_fit
         error("`imp` must be fit first.")
     end
